bd2902fcf25d6536baceb335cee0a84714ce4aad
Dechao Meng <dechao.meng@vipl.ict.ac.cn>
2021-01-28 20:17:43 +0800

    add video2frame

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
+import glob
 
 setup(
     name="mdc_tools",
-    version="1.2",
+    version="1.3",
     author="Dechao Meng",
     author_email="dechao.meng@vipl.ict.ac.cn",
     url="https://github.com/silverbulletmdc/mdc_tools",
     # keywords=("pytorch", "vehicle", "ReID"),
     description="Personal tools",
+    scripts=glob.glob('scripts/*'),
     # long_description="",
     packages=find_packages(exclude=('examples', 'examples.*')),
 )
