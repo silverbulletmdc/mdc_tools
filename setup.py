4114d9292dfb9a5e9e2b520b4a724a20fcbe6c4a
Dechao Meng <dechao.meng@vipl.ict.ac.cn>
2021-06-21 19:46:39 +0800

    add frame2video

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import glob
 
 setup(
     name="mdc_tools",
-    version="1.3.7",
+    version="1.3.8",
     author="Dechao Meng",
     author_email="dechao.meng@vipl.ict.ac.cn",
     url="https://github.com/silverbulletmdc/mdc_tools",
